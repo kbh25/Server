b73a0d5311dba3e1ac59238c7daec1f4fc220c2e
Khaldoun <bh.clothing@outlook.com>
2023-05-05 09:44:59 +0300

    new changes

--- routes/index.js
+++ routes/index.js
@@ -75,40 +75,106 @@ router.post('/register', function(request, response, next){
     var Balance = request.body.Balance;
 
 
+    
     if(FirstName && LastName && PhoneNumber && Email && Password && DOB && Gender)
-    {
-        /*var query = 
-        'INSERT INTO register (FirstName, LastName, PhoneNumber, Email, Password, DOB, Gender) VALUES ("${first_Name}","${LastName}", "${PhoneNumber}", "${Email}", "${Password}", "${DOB}", "${Gender};")';
-          */
+    { 
         var query = ` Insert INTO register (FirstName, LastName, PhoneNumber, Email, Password, DOB, Gender) VALUES ("${First_Name}", "${LastName}" , "${PhoneNumber}" , "${Email}" , "${Password}" , "${DOB}", "${Gender}")`;
         database.query(query,function(error,data){
-
+           
             if(error){
                 throw error;
             }
             else{
-                response.redirect("/home.ejs");
+                response.json(FirstName);
+                response.json(LastName);
+                response.json(PhoneNumber);
+                response.json(Email);
+                response.json(Password);
+                response.json(DOB);
+                response.json(Gender);
             }
-        });
+            
+        }); 
     }
     });
-/*      check if upper syntax or this one
-  connection.query(sql, [firstname, lastname, password, email, dateOfBirth, balance, gender], (error, result) => {
-    if (error) {
-      console.error('Error inserting user into MySQL database:', error);
-      response.status(500).json({ error: 'Internal server error' });
-    } else {
-      console.log('User registered:', result);
-      response.status(200).json({ message: 'User registered successfully' });
-    }
+    router.post('/reserve', function(request, response, next){
+
+        var res_Date = request.body.res_Date;
+        var res_Time = request.body.res_Time;
+        var Duration = request.body.Duration;
+        var vehicle_type = request.body.vehicle_type;
+        var vehicle_number = request.body.vehicle_number;
+        if(Date && Time && Duration && Vehicle_Type && Vehicle_Number)
+        {
+            var query = ` Insert INTO reservation (res_Date, res_Time, Duration, vehicle_type, vehicle_number) VALUES ("${res_Date}", "${res_Time}" , "${Duration}" , "${vehicle_type}" , "${vehicle_number}")`;
+            database.query(query,function(error,data){
+    
+                if(error){
+                    throw error;
+                }
+                else{
+                    response.redirect("/home.ejs");
+                }
+            }); 
+        }
+        });
+
+        // inserting money to customer table by ID
+
+        router.post('/users/:id/funds/insert', (req, res) => {
+            const userId = req.params.id;
+            const amount = req.body.amount;
+
+            var query = ` INSERT INTO register( Balance ) VALUES ( "${amount}" ) WHERE id = ("${userId}"); database.query(query,function(error,data)`
+    
+                if(error){
+                    throw error;
+                }
+                else{
+                    response.redirect("/home.ejs");
+                }
+            }); 
+
+                // deducting money so that customer reserves with conditions
+
+            router.post('/users/:id/funds/deduct', (req, res) => {
+                const userId = req.params.id;
+                const amount = req.body.amount;
+                
+
+                    connection.query( ` SELECT Balance FROM register WHERE id = ("${userId}"); `, function (error, results, fields) {
+                        if (error) throw error;
+                        
+                        // store the column data in a variable
+                        const amount = results.map(result => result.column_name);
+                      
+                        console.log(columnData); // output the column data to the console
+                      });
+
+
+                    return res.status(400).json({ error: 'Balance is zero or negative' });
+                  
+                var query = ` INSERT INTO register( Balance ) VALUES ( "${amount}" ) WHERE id = ("${userId}"); database.query(query,function(error,data)`
+        
+                    if(error){
+                        throw error;
+                    }
+                    else{
+                        response.redirect("/home.ejs");
+                    }
+                }); 
+
+// code to retrieve balance of each user
+router.get('/users/:id/balance', (req, res) => {
+    connection.query('SELECT balance FROM register', (error, results, fields) => {
+      if (error) throw error;
+      const balance = results[0].balance;
+      res.send(balance);
+    });
   });
 
 
 
-
-}*/
-
-    
-
-
+       
+ 
 module.exports = router;
